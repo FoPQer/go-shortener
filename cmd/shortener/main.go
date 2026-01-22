424db7414cea58193fa4336ef892881b38917be3
FoPer <iritskiy.sas@gmail.com>
2026-01-22 22:48:52 +0300

    ready to PR

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,50 @@
 package main
 
-func main() {}
+import (
+	"crypto/rand"
+	"io"
+	"net/http"
+	"strings"
+)
+
+var urls map[string]string
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			http.Error(res, "", 400)
+			return
+		}
+
+		id := rand.Text()[0:8]
+		urls[id] = string(body)
+
+		res.WriteHeader(http.StatusCreated)
+		res.Write([]byte("http://localhost:8080/" + id))
+	case http.MethodGet:
+		splitted_path := strings.Split(strings.TrimPrefix(req.URL.Path, "/"), "/")
+		if len(splitted_path) > 1 {
+			http.Error(res, "", 400)
+			return
+		}
+
+		url, ok := urls[splitted_path[0]]
+		if !ok {
+			http.Error(res, "", 400)
+			return
+		}
+
+		res.WriteHeader(307)
+		res.Write([]byte(url))
+	}
+
+}
+
+func main() {
+	urls = make(map[string]string)
+	if err := http.ListenAndServe(":8080", http.HandlerFunc(mainPage)); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/FoPQer/go-shortener
+
+go 1.23.11
